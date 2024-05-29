5110713921eae7d8b2439046eda728a1e29ddb4a
kalmar13 <yhspz5ba8c4rxd@int.pl>
2024-05-29 15:57:28 -0700

    poprawka

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+"""kom dla lint"""
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """kom dla lint"""
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 if __name__ == "__main__":
